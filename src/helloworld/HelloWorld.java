eb5c4bb28b49a7b0df953edf0dd221d7887933f1
g13942sy <g13942sy@s30420.tsuda.ac.jp>
2015-04-10 11:34:28 +0900

    Hello,world出力

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g13942sy
+ */
+public class HelloWorld {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("!dlroW,olleH");
+    }
+}
